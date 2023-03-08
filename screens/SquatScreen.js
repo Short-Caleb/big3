45fb526222ae572e9f063411039af77a914f881a
Caleb Short <shortcaleb@gmail.com>
2023-03-08 16:06:43 -0500

    8 mar commit

--- screens/SquatScreen.js
+++ screens/SquatScreen.js
@@ -1,12 +1,78 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Pressable, ImageBackground } from 'react-native';
+import { useEffect, useState } from 'react';
+import { StyleSheet, Text, View, Pressable, ImageBackground, TextInput, Button, FlatList } from 'react-native';
 import bg from '../assets/weight.jpg';
 
+
+const [squatInput, setSquatInput] = useState('');
+const [squatData, setSquatData] = useState({});
+ 
 const SquatScreen = ({navigation}) => {
+
+useEffect(() => {
+  async function restoreData() {
+    let permaData;
+    permaData = await getData();
+    setSquatData(permaData);
+  }
+  restoreData()
+}, [])
+
+const addSquatWorkout = () => {
+  squatData.messages.push({text: squatInput});
+  setSquatInput('');
+  console.log(squatData.messages);
+
+  storeData()
+};
+
+const renderItem = ({item}) => (
+  <View>
+    <Text>{item.text}</Text>
+  </View>
+)
+
+  const storeData = async (value) => {
+    try {
+      const jsonValue = JSON.stringify(value)
+      await AsyncStorage.setItem('@storage_Key', jsonValue)
+    } catch (e) {
+      // saving error
+      console.log(e)
+      
+    }
+  }
+
+
+  const getData = async () => {
+    try {
+      const jsonValue = await AsyncStorage.getItem('@storage_Key')
+      return jsonValue != null ? JSON.parse(jsonValue) : null;
+    } catch(e) {
+      // error reading value
+    }
+  }
+  
   return (
     <View style={styles.container}>
       <ImageBackground source={bg} resizeMode="cover" style={styles.bgImage} >
       <Text style={{color: 'white'}}>SquatScreen</Text>
+      <TextInput 
+        editable
+        multiline
+        numberOfLines={2}
+        maxLength={100}
+        onChangeText={setSquatInput}
+        value={squatInput}
+      />
+
+    <Button   title='Add workout' onPress={addSquatWorkout}/>
+
+    <FlatList
+    data={squatData.messages}
+    renderItem={renderItem}
+    keyExtractor={(item, index) => index}
+    />
 
       <Pressable style={styles.backButton} onPress={() => navigation.goBack()}>
         <Text style={{color: 'white'}}>Back</Text>
